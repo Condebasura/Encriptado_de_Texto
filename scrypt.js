55be94d769779f31ac37c26785591427c62deba4
Pablo Zabala <could_2032@outlook.com>
2022-07-17 05:01:15 -0300

    creando las variables

---
+++ scrypt.js
@@ -0,0 +1,7 @@
+let campoDesencriptado = document.querySelector(".campo_de__texto");
+
+const botonEncriptar = document.querySelector(".button__encriptado");
+
+const botonDesencriptar = document.querySelector(".button__desencriptar");
+
+let campoEncriptado = document.querySelector(".campo__resultado");
