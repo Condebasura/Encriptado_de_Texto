82e1a16720914dfa551511475795adeced7c0d30
Pablo Zabala <could_2032@outlook.com>
2022-07-17 05:30:03 -0300

    terminando de crear las variables

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,13 @@
-let campoDesencriptado = document.querySelector(".campo_de__texto");
+let campoEncriptar = document.querySelector(".campo_de__texto");
 
 const botonEncriptar = document.querySelector(".button__encriptado");
 
 const botonDesencriptar = document.querySelector(".button__desencriptar");
 
-let campoEncriptado = document.querySelector(".campo__resultado");
+let campoDesencriptar = document.querySelector(".campo__resultado");
+
+let cardNada = document.querySelector(".card");
+
+let cardResult = document.querySelector(".card2");
+
+const botonCopiar = document.querySelector(".boton_copiar"); 
