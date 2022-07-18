12bc8ecb5ea1370c113c10641f645f1a0c8c52ce
Pablo Zabala <could_2032@outlook.com>
2022-07-18 03:15:10 -0300

    pausando en scrypt

--- scrypt.js
+++ scrypt.js
@@ -1,13 +1,17 @@
-let campoEncriptar = document.querySelector(".campo_de__texto");
+const campoTexto = document.querySelector("[data-form-text]");
 
-const botonEncriptar = document.querySelector(".button__encriptado");
+const botonEncriptar = document.querySelector("[data-from-btnEnc]");
 
 const botonDesencriptar = document.querySelector(".button__desencriptar");
 
-let campoDesencriptar = document.querySelector(".campo__resultado");
+const campoResultado = document.querySelector(".campo__resultado");
 
-let cardNada = document.querySelector(".card");
+const cardNada = document.querySelector(".card");
 
-let cardResult = document.querySelector(".card2");
+const cardResult = document.querySelector(".resultado");
 
 const botonCopiar = document.querySelector(".boton_copiar"); 
+
+botonEncriptar.addEventListener("click", function(evento){
+    console.log(campoTexto.value);
+    })
